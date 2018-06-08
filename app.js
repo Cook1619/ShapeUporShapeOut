91e91b5092c8443e006288acc7af3aca53ea8844
Cook1619 <cook6348@gmail.com>
2018-06-08 10:08:05 -0500

    Few minor additions

--- app.js
+++ app.js
@@ -1,29 +1,33 @@
 class Shape {
     constructor(){
-
+        
     }
 }
 
 class Circle extends Shape{
     constructor(){
+        super()
 
     }
 }
 
 class Triangle extends Shape {
     constructor() {
+        super()
 
     }
 }
 
 class Rectangle extends Shape {
     constructor() {
+        super()
 
     }
 }
 
 class Square extends Shape {
     constructor() {
+        super()
 
     }
 }
