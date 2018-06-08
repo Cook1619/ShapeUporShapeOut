c2710cb73c6ae3d131829aaa27470fd313b3405f
Cook1619 <cook6348@gmail.com>
2018-06-08 09:15:15 -0500

    More setting up html and css, also the skeleton for my JS classes

---
+++ app.js
@@ -0,0 +1,30 @@
+class Shape {
+    constructor(){
+
+    }
+}
+
+class Circle extends Shape{
+    constructor(){
+
+    }
+}
+
+class Triangle extends Shape {
+    constructor() {
+
+    }
+}
+
+class Rectangle extends Shape {
+    constructor() {
+
+    }
+}
+
+class Square extends Shape {
+    constructor() {
+
+    }
+}
+
