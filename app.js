5b2a7cc7fa803dc08b45b523c3bef193bc949f48
Cook1619 <cook6348@gmail.com>
2018-06-10 11:34:44 -0500

    added event listeners on all buttons

--- app.js
+++ app.js
@@ -1,33 +1,59 @@
+let rectBtn =  document.getElementById('create-rectangle');
+rectBtn.addEventListener('click', function(){
+    alert('hello');
+})
+let squaretBtn = document.getElementById('create-square');
+squareBtn.addEventListener('click', function () {
+    alert('hello');
+})
+let circleBtn = document.getElementById('create-circle');
+circleBtn.addEventListener('click', function () {
+    alert('hello');
+})
+let triangleBtn = document.getElementById('create-right-triangle');
+triangleBtn.addEventListener('click', function () {
+    alert('hello');
+})
+
+
 class Shape {
-    constructor(){
-        
+    constructor(width, height, background){
+        this.width = width;
+        this.height = height;
+        this.background = background;
+    }
+    removeShape(){
+
+    }
+    describe(){
+
     }
 }
 
 class Circle extends Shape{
-    constructor(){
-        super()
-
+    constructor(width, height, background, radius){
+        super(width, height, background)
+        this.radius = radius;
     }
 }
 
 class Triangle extends Shape {
-    constructor() {
-        super()
+    constructor(width, height, background) {
+        super(width, height, background)
 
     }
 }
 
 class Rectangle extends Shape {
-    constructor() {
-        super()
+    constructor(width, height, background) {
+        super(width, height, background)
 
     }
 }
 
-class Square extends Shape {
-    constructor() {
-        super()
+class Square extends Rectangle {
+    constructor(width, height, background) {
+        super(width, height, background)
 
     }
 }
